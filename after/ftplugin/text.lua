e8b553576c492d7941ae64c47952f3c7b1a12755
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-01-07 23:46:12 -0500

    Updated text ftplugin file

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,3 +1,10 @@
+vim.opt_local.wrap = true
+
+vim.opt_local.spell = true
+
+vim.opt_local.colorcolumn = ""
+vim.opt_local.sidescrolloff = 12
+
 local km = require("mjm.keymap_mod")
 
 vim.keymap.set("i", "<backspace>", function()
