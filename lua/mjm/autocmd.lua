073c5b55866a8051819e632f0833c64307e690f7
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-01-10 11:28:54 -0500

    Refactor backup formatter and add fixes for misalignment with shiftwidth

--- lua/mjm/autocmd.lua
+++ lua/mjm/autocmd.lua
@@ -75,43 +75,39 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         end
 
         local expandtab = vim.api.nvim_buf_get_option(ev.buf, "expandtab")
+        local shiftwidth = vim.api.nvim_buf_get_option(ev.buf, "shiftwidth")
+
+        if shiftwidth == 0 then
+            shiftwidth = vim.api.nvim_buf_get_option(ev.buf, "tabstop")
+        else
+            vim.api.nvim_buf_set_option(ev.buf, "tabstop", shiftwidth)
+        end
 
         if expandtab then
-            local shiftwidth = vim.api.nvim_buf_get_option(ev.buf, "shiftwidth")
-
-            if shiftwidth == 0 then
-                shiftwidth = vim.api.nvim_buf_get_option(ev.buf, "tabstop")
-            else
-                vim.api.nvim_buf_set_option(ev.buf, "tabstop", shiftwidth)
-            end
-
             vim.api.nvim_buf_set_option(ev.buf, "softtabstop", shiftwidth)
             vim.api.nvim_exec2(ev.buf .. "bufdo retab", {})
         end
 
-        local leading_blanks = true
-
-        while leading_blanks do
+        local function top_blank_lines()
             local top_line = vim.api.nvim_buf_get_lines(ev.buf, 0, 1, true)[1]
 
             if top_line == "" then
                 vim.api.nvim_buf_set_lines(ev.buf, 0, 1, false, {})
-            else
-                leading_blanks = false
+                top_blank_lines()
             end
         end
 
-        local trailing_blanks = true
-
-        while trailing_blanks do
+        local function bottom_blank_lines()
             local bottom_line = vim.api.nvim_buf_get_lines(ev.buf, -2, -1, true)[1]
 
             if bottom_line == "" then
                 vim.api.nvim_buf_set_lines(ev.buf, -2, -1, false, {})
-            else
-                trailing_blanks = false
+                bottom_blank_lines()
             end
         end
+
+        top_blank_lines()
+        bottom_blank_lines()
 
         local total_lines = vim.api.nvim_buf_line_count(ev.buf)
         local lines = vim.api.nvim_buf_get_lines(ev.buf, 0, total_lines, true)
@@ -119,30 +115,63 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         local consecutive_blanks = 0
         local lines_removed = 0
 
-        for i, line in ipairs(lines) do
-            local is_blank = line:match("^%s*$")
+        local format_line = function(iter, line)
+            local empty_line = line == ""
+            local whitespace_line = line:match("^%s*$")
+            local blank_line = empty_line or whitespace_line
 
-            if line == "" or is_blank then
+            if blank_line then
                 consecutive_blanks = consecutive_blanks + 1
-                local row = i - lines_removed - 1
-
-                if consecutive_blanks > 1 then
-                    vim.api.nvim_buf_set_lines(ev.buf, row, row + 1, false, {})
-                    lines_removed = lines_removed + 1
-                elseif is_blank then
-                    vim.api.nvim_buf_set_text(ev.buf, row, 0, row, #line, {})
-                end
-            else
-                consecutive_blanks = 0
-
-                local line_length = #line
-                local row = i - 1 - lines_removed
-                local last_non_blank, _ = line:find("(%S)%s*$")
-
-                if last_non_blank and last_non_blank ~= line_length then
-                    vim.api.nvim_buf_set_text(ev.buf, row, last_non_blank, row, line_length, {})
-                end
             end
+
+            local row = iter - lines_removed - 1
+
+            if blank_line and consecutive_blanks > 1 then
+                vim.api.nvim_buf_set_lines(ev.buf, row, row + 1, false, {})
+                lines_removed = lines_removed + 1
+
+                return
+            end
+
+            if whitespace_line then
+                vim.api.nvim_buf_set_text(ev.buf, row, 0, row, #line, {})
+
+                return
+            end
+
+            consecutive_blanks = 0
+
+            local line_length = #line
+            local last_non_blank, _ = line:find("(%S)%s*$")
+
+            if last_non_blank and last_non_blank ~= line_length then
+                vim.api.nvim_buf_set_text(ev.buf, row, last_non_blank, row, line_length, {})
+            end
+
+            local first_non_blank, _ = line:find("%S") or 1, nil
+
+            first_non_blank = first_non_blank - 1
+            local extra_spaces = first_non_blank % shiftwidth
+
+            if extra_spaces == 0 or not expandtab then
+                return
+            end
+
+            local half_shiftwidth = shiftwidth * 0.5
+            local round_up = extra_spaces >= half_shiftwidth
+
+            if round_up then
+                local new_spaces = shiftwidth - extra_spaces
+                local spaces = string.rep(" ", new_spaces)
+
+                vim.api.nvim_buf_set_text(ev.buf, row, 0, row, 0, { spaces })
+            else
+                vim.api.nvim_buf_set_text(ev.buf, row, 0, row, extra_spaces, {})
+            end
+        end
+
+        for i, line in ipairs(lines) do
+            format_line(i, line)
         end
     end,
 })
