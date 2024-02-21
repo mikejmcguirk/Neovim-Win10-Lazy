5d6c0b428bd13d82095bc2f44f4dd4e4a4dc0154
mikejmcguirk <mike.j.mcguirk@gmail.com>
2024-02-21 10:03:28 -0500

    Added insert backspace fix with blanks to sql ftplugin

--- ftplugin/sql.lua
+++ ftplugin/sql.lua
@@ -1,3 +1,9 @@
 -- Disable default SQL ftplugin file
 vim.b.did_ftplugin = 1
 vim.b.current_ftplugin = "sql"
+
+local km = require("mjm.keymap_mod")
+
+vim.keymap.set("i", "<backspace>", function()
+    km.insert_backspace_fix({ allow_blank = true })
+end, { silent = true, buffer = true })
