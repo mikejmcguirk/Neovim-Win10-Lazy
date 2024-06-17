cfac664a2ec8c1dc5e8640e5ebc5568e16158562
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-06-17 17:14:53 -0400

    Updated backspace logic for text files in bulleted lists

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -6,10 +6,6 @@ vim.opt_local.colorcolumn = ""
 vim.opt_local.sidescrolloff = 12
 
 vim.keymap.set("i", "<backspace>", function()
-    -- Add case where there is text after the bullet but you are on or right after the bullet
-    -- This should push the bullet back
-    -- shouldn't we also be able to handle the case where we have extra spaces after the bullet
-    -- Should just be able to hit <bs> once to remove them
     local cur_line = vim.api.nvim_get_current_line()
     local start_idx, end_idx = string.find(cur_line, "%S")
     if not start_idx then
@@ -17,60 +13,67 @@ vim.keymap.set("i", "<backspace>", function()
         return
     end
 
-    local first_two = string.sub(cur_line, start_idx, end_idx + 1)
-    local is_list = first_two == "- " or first_two == "-"
-    if not is_list then
+    -- Are we in a bulleted list?
+    local first_one = string.sub(cur_line, start_idx, start_idx)
+    if first_one ~= "-" then
         bp.insert_backspace_fix({ allow_blank = true })
         return
     end
 
     local start_row, start_col = unpack(vim.api.nvim_win_get_cursor(0))
-    if start_col < start_idx and start_idx > 1 then
+    if start_col >= start_idx + 2 then
         bp.insert_backspace_fix({ allow_blank = true })
         return
     end
 
-    local is_at_end = #cur_line == end_idx or #cur_line == end_idx + 1
-    if not is_at_end then
-        bp.insert_backspace_fix({ allow_blank = true })
-        return
-    end
-
-    local whitespace = start_idx - 1
     local edit_row = start_row - 1
-    if whitespace and is_at_end == 0 then
+    local second_one = string.sub(cur_line, start_idx + 1, start_idx + 1)
+    -- Better to handle this case separately and early
+    -- Addressing it within the rest of the logic gets convoluted
+    if (#cur_line == 2 and second_one == " ") or #cur_line == 1 then
         vim.api.nvim_buf_set_text(0, edit_row, 0, edit_row, #cur_line, { "" })
         return
     end
 
-    local shiftwidth = vim.fn.shiftwidth()
-    if whitespace <= shiftwidth then
-        vim.api.nvim_buf_set_text(0, edit_row, 0, edit_row, whitespace, { "" })
-        if string.len(first_two) == 2 and start_col == start_idx then
-            local key = vim.api.nvim_replace_termcodes("<right>", true, false, true)
-            vim.api.nvim_feedkeys(key, "n", false)
-        end
+    -- Avoid edge case where there is no leading whitespace and you backspace twice into
+    -- the previous line
+    if start_col == 0 then
+        bp.insert_backspace_fix({ allow_blank = true })
         return
     end
 
-    local extra_spaces = whitespace % shiftwidth
+    -- These feedkeys are not theoretically necessary in cases where leading whitespace is zero
+    -- However, by doing them anyway, we avoid creating special logic to handle one case
+    -- And I see no performance issue with the extra feedkey
+    if start_col == start_idx and (not second_one or second_one ~= " ") then
+        local key = vim.api.nvim_replace_termcodes("<space>", true, false, true)
+        vim.api.nvim_feedkeys(key, "n", false)
+    elseif start_col == start_idx and second_one == " " then
+        local key = vim.api.nvim_replace_termcodes("<right>", true, false, true)
+        vim.api.nvim_feedkeys(key, "n", false)
+    end
+
+    local whitespace = start_idx - 1
+    local shiftwidth = vim.fn.shiftwidth()
+    if whitespace == 0 then
+        local key = vim.api.nvim_replace_termcodes("<bs><bs>", true, false, true)
+        vim.api.nvim_feedkeys(key, "n", false)
+        return
+    end
+
+    if whitespace <= shiftwidth then
+        vim.api.nvim_buf_set_text(0, edit_row, 0, edit_row, whitespace, { "" })
+        return
+    end
+
     local to_remove = nil
+    local extra_spaces = whitespace % shiftwidth
     if extra_spaces == 0 then
         to_remove = shiftwidth
     else
         to_remove = extra_spaces
     end
-
     vim.api.nvim_buf_set_text(0, edit_row, 0, edit_row, to_remove, { "" })
-    if first_two == "-" then
-        local key = vim.api.nvim_replace_termcodes("<space>", true, false, true)
-        vim.api.nvim_feedkeys(key, "n", false)
-        return
-    end
-    if string.len(first_two) == 2 and start_col == start_idx then
-        local key = vim.api.nvim_replace_termcodes("<right>", true, false, true)
-        vim.api.nvim_feedkeys(key, "n", false)
-    end
 end, { silent = true, buffer = true })
 
 vim.keymap.set("i", "<cr>", function()
