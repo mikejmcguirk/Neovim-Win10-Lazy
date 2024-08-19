9a03aa4bc7183dbe598f915e92eddf2f95c2fe87
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-08-18 22:50:30 -0400

    Added obsidian.nvim. Removed nvim-lint

--- lua/mjm/plugins/format_lint.lua
+++ lua/mjm/plugins/format_lint.lua
@@ -14,22 +14,4 @@ return {
             })
         end,
     },
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufWritePre", "BufNewFile" },
-        config = function()
-            local lint = require("lint")
-            lint.linters_by_ft = {
-                markdown = { "markdownlint" },
-            }
-
-            vim.api.nvim_create_autocmd("BufWritePost", {
-                group = vim.api.nvim_create_augroup("lint_group", { clear = true }),
-                pattern = "*",
-                callback = function()
-                    lint.try_lint()
-                end,
-            })
-        end,
-    },
 }
