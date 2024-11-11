44d1a9068e719a3fe4a6939ddfdcb1c619638c38
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-11-11 14:23:19 -0500

    pare down colorizer config

--- lua/mjm/plugins/colorizer.lua
+++ lua/mjm/plugins/colorizer.lua
@@ -3,24 +3,9 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("colorizer").setup({
-            filetypes = { "*" },
             user_default_options = {
-                RGB = true,
-                RRGGBB = true,
                 names = false,
-                RRGGBBAA = false,
-                AARRGGBB = false,
-                rgb_fn = false,
-                hsl_fn = false,
-                css = false,
-                css_fn = false,
-                mode = "background",
-                tailwind = false,
-                sass = { enable = false, parsers = { "css" } },
-                virtualtext = "■",
-                always_update = false,
             },
-            buftypes = {},
         })
 
         vim.keymap.set("n", "<leader>ot", "<cmd>ColorizerToggle<cr>")
