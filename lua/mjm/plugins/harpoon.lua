c0e16e4687872df0efaf3a3db07a35d8a252b2ca
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-11-15 00:37:02 -0500

    Commented out custom select harpoon function. Will see if it works

--- lua/mjm/plugins/harpoon.lua
+++ lua/mjm/plugins/harpoon.lua
@@ -16,77 +16,77 @@ return {
                         height = 10,
                     },
                 },
-                default = {
-                    -- The default function uses bufload instead of edit to open the buffer
-                    -- I cannot remember what it was now, but it caused some kind of
-                    -- serious issue. The logic is re-written to use edit like Telescope does
-                    select = function(list_item, list, options)
-                        logger:log("config_default#select", list_item, list.name, options)
-                        if list_item == nil then
-                            return
-                        end
-
-                        local bufnr = vim.fn.bufnr(list_item.value)
-                        if vim.api.nvim_get_current_buf() == bufnr then
-                            vim.notify("Already in buffer")
-                            return
-                        end
-
-                        local set_position = false
-                        if bufnr == -1 then
-                            set_position = true
-                        end
-
-                        options = options or {}
-                        if options.vsplit then
-                            vim.cmd("vsplit")
-                        elseif options.split then
-                            vim.cmd("split")
-                        elseif options.tabedit then
-                            vim.cmd("tabedit")
-                        end
-
-                        vim.api.nvim_exec2("edit " .. list_item.value, {})
-                        bufnr = vim.fn.bufnr(list_item.value)
-
-                        if set_position then
-                            local lines = vim.api.nvim_buf_line_count(bufnr)
-
-                            local edited = false
-                            if list_item.context.row > lines then
-                                list_item.context.row = lines
-                                edited = true
-                            end
-
-                            local row = list_item.context.row
-                            local row_text = vim.api.nvim_buf_get_lines(0, row - 1, row, false)
-                            local col = #row_text[1]
-
-                            if list_item.context.col > col then
-                                list_item.context.col = col
-                                edited = true
-                            end
-
-                            vim.api.nvim_win_set_cursor(0, {
-                                list_item.context.row or 1,
-                                list_item.context.col or 0,
-                            })
-
-                            if edited then
-                                extensions.extensions:emit(
-                                    extensions.event_names.POSITION_UPDATED,
-                                    {
-                                        list_item = list_item,
-                                    }
-                                )
-                            end
-                        end
-
-                        extensions.extensions:emit(extensions.event_names.NAVIGATE, {
-                            buffer = bufnr,
-                        })
-                    end,
-                },
+                -- default = {
+                --     -- The default function uses bufload instead of edit to open the buffer
+                --     -- I cannot remember what it was now, but it caused some kind of
+                --     -- serious issue. The logic is re-written to use edit like Telescope does
+                --     select = function(list_item, list, options)
+                --         logger:log("config_default#select", list_item, list.name, options)
+                --         if list_item == nil then
+                --             return
+                --         end
+                --
+                --         local bufnr = vim.fn.bufnr(list_item.value)
+                --         if vim.api.nvim_get_current_buf() == bufnr then
+                --             vim.notify("Already in buffer")
+                --             return
+                --         end
+                --
+                --         local set_position = false
+                --         if bufnr == -1 then
+                --             set_position = true
+                --         end
+                --
+                --         options = options or {}
+                --         if options.vsplit then
+                --             vim.cmd("vsplit")
+                --         elseif options.split then
+                --             vim.cmd("split")
+                --         elseif options.tabedit then
+                --             vim.cmd("tabedit")
+                --         end
+                --
+                --         vim.api.nvim_exec2("edit " .. list_item.value, {})
+                --         bufnr = vim.fn.bufnr(list_item.value)
+                --
+                --         if set_position then
+                --             local lines = vim.api.nvim_buf_line_count(bufnr)
+                --
+                --             local edited = false
+                --             if list_item.context.row > lines then
+                --                 list_item.context.row = lines
+                --                 edited = true
+                --             end
+                --
+                --             local row = list_item.context.row
+                --             local row_text = vim.api.nvim_buf_get_lines(0, row - 1, row, false)
+                --             local col = #row_text[1]
+                --
+                --             if list_item.context.col > col then
+                --                 list_item.context.col = col
+                --                 edited = true
+                --             end
+                --
+                --             vim.api.nvim_win_set_cursor(0, {
+                --                 list_item.context.row or 1,
+                --                 list_item.context.col or 0,
+                --             })
+                --
+                --             if edited then
+                --                 extensions.extensions:emit(
+                --                     extensions.event_names.POSITION_UPDATED,
+                --                     {
+                --                         list_item = list_item,
+                --                     }
+                --                 )
+                --             end
+                --         end
+                --
+                --         extensions.extensions:emit(extensions.event_names.NAVIGATE, {
+                --             buffer = bufnr,
+                --         })
+                --     end,
+                -- },
             })
 
             vim.keymap.set("n", "<leader>ad", function()
