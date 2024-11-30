5b3844c2fbfd78996a8b5805a01db5da385b9149
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-11-29 19:46:17 -0500

    re-added custom harpoon select function

--- lua/mjm/plugins/harpoon.lua
+++ lua/mjm/plugins/harpoon.lua
@@ -10,6 +10,8 @@ return {
         },
         config = function()
             local harpoon = require("harpoon")
+            local logger = require("harpoon.logger")
+            local extensions = require("harpoon.extensions")
 
             harpoon:setup({
                 settings = {
@@ -18,6 +20,76 @@ return {
                     menu = {
                         height = 10,
                     },
+                },
+                default = {
+                    -- When using the in-build select function, ftplugin settings fail to load
+                    -- Issue does not occur if we use edit instead
+                    select = function(list_item, list, options)
+                        logger:log("config_default#select", list_item, list.name, options)
+                        if list_item == nil then
+                            return
+                        end
+
+                        local bufnr = vim.fn.bufnr(list_item.value)
+                        if vim.api.nvim_get_current_buf() == bufnr then
+                            vim.notify("Already in buffer")
+                            return
+                        end
+
+                        local set_position = false
+                        if bufnr == -1 then
+                            set_position = true
+                        end
+
+                        options = options or {}
+                        if options.vsplit then
+                            vim.cmd("vsplit")
+                        elseif options.split then
+                            vim.cmd("split")
+                        elseif options.tabedit then
+                            vim.cmd("tabedit")
+                        end
+
+                        vim.api.nvim_exec2("edit " .. list_item.value, {})
+                        bufnr = vim.fn.bufnr(list_item.value)
+
+                        if set_position then
+                            local lines = vim.api.nvim_buf_line_count(bufnr)
+
+                            local edited = false
+                            if list_item.context.row > lines then
+                                list_item.context.row = lines
+                                edited = true
+                            end
+
+                            local row = list_item.context.row
+                            local row_text = vim.api.nvim_buf_get_lines(0, row - 1, row, false)
+                            local col = #row_text[1]
+
+                            if list_item.context.col > col then
+                                list_item.context.col = col
+                                edited = true
+                            end
+
+                            vim.api.nvim_win_set_cursor(0, {
+                                list_item.context.row or 1,
+                                list_item.context.col or 0,
+                            })
+
+                            if edited then
+                                extensions.extensions:emit(
+                                    extensions.event_names.POSITION_UPDATED,
+                                    {
+                                        list_item = list_item,
+                                    }
+                                )
+                            end
+                        end
+
+                        extensions.extensions:emit(extensions.event_names.NAVIGATE, {
+                            buffer = bufnr,
+                        })
+                    end,
                 },
             })
 
