1fb1816229a5987b0e57ee2e4152252afe0b2aae
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-12-08 11:13:08 -0500

    Added obsidian macro for date entry

--- lua/mjm/plugins/obsidian.lua
+++ lua/mjm/plugins/obsidian.lua
@@ -269,5 +269,14 @@ return {
         vim.keymap.set("n", "]o", function()
             next_file_in_dir({ goto_next = true, skip_extensions })
         end)
+
+        vim.keymap.set("n", "<leader>sw", function()
+            local year = os.date("%Y")
+            local month = os.date("%m")
+            local day = os.date("%d")
+            local date = year .. "-" .. month .. "-" .. day
+
+            return "Go" .. date .. ",,<left><space>"
+        end, { expr = true })
     end,
 }
