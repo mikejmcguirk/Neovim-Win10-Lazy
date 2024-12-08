f4e158b195b514f100b190230f3acfe01dfcbdd2
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-12-07 22:12:06 -0500

    Added indent padding to lua annotations

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,18 +1,21 @@
+local ut = require("mjm.utils")
+
 ---@param annotation string
 ---@return nil
 local function add_annotation(annotation)
-    local buf = vim.api.nvim_get_current_buf() ---@type integer
     local row = vim.api.nvim_win_get_cursor(0)[1] ---@type integer
     local line = vim.api.nvim_get_current_line()
     local line_len = #line ---@type integer
 
     if line:match("^%s*$") then
-        vim.api.nvim_buf_set_text(buf, row - 1, 0, row - 1, 0, { annotation .. " " })
+        local indent = ut.get_indent(row) ---@type integer
+        local padding = string.rep(" ", indent) ---@type string
+        vim.api.nvim_buf_set_text(0, row - 1, 0, row - 1, 0, { padding .. annotation .. " " })
     elseif line:match("%s$") then
-        vim.api.nvim_buf_set_text(buf, row - 1, line_len, row - 1, line_len, { annotation .. " " })
+        vim.api.nvim_buf_set_text(0, row - 1, line_len, row - 1, line_len, { annotation .. " " })
     else
         vim.api.nvim_buf_set_text(
-            buf,
+            0,
             row - 1,
             line_len,
             row - 1,
