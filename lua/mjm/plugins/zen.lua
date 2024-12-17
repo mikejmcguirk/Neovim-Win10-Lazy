8770d50bc6c552db5ff7d48553398eedcbe4e85f
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-12-17 02:10:37 -0500

    improved zen tmux handling to cover more cases

--- lua/mjm/plugins/zen.lua
+++ lua/mjm/plugins/zen.lua
@@ -1,3 +1,7 @@
+local tmux_safety = vim.api.nvim_create_augroup("tmux_safety", { clear = true })
+
+-- Anything tmux related is handled manually because, by default, when zen-mode closes, it also
+-- unhides all tmux panes
 return {
     "folke/zen-mode.nvim",
     opts = {
@@ -10,13 +14,30 @@ return {
             },
         },
         on_open = function()
-            vim.api.nvim_create_autocmd({ "QuitPre", "VimLeave" }, {
-                group = vim.api.nvim_create_augroup("tmux_safety", { clear = true }),
+            vim.api.nvim_clear_autocmds({ group = "tmux_safety" })
+
+            vim.api.nvim_create_autocmd({ "VimLeave" }, {
+                group = tmux_safety,
+                once = true,
                 pattern = "*",
                 callback = function()
                     pcall(function()
                         vim.fn.system([[tmux set status on]])
                     end)
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("WinClosed", {
+                group = tmux_safety,
+                once = true,
+                pattern = "*",
+                callback = function(event)
+                    local closed_win = tonumber(event.match)
+                    if closed_win == require("zen-mode.view").win then
+                        pcall(function()
+                            vim.fn.system([[tmux set status on]])
+                        end)
+                    end
                 end,
             })
         end,
