b08e4982c4a8bc5814ff48f8db096fe786948ad4
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-12-23 09:24:32 -0500

    Got rid of unneeded harpoon code. Moved mark assignments to one loop

--- lua/mjm/plugins/harpoon.lua
+++ lua/mjm/plugins/harpoon.lua
@@ -4,9 +4,7 @@ return {
         branch = "harpoon2",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            -- "jasonpanosso/harpoon-tabline.nvim",
             "mike-jl/harpoonEx",
-            -- "mikejmcguirk/harpoonEx",
         },
         config = function()
             local harpoon = require("harpoon")
@@ -22,7 +20,7 @@ return {
                     },
                 },
                 default = {
-                    -- When using the in-build select function, ftplugin settings fail to load
+                    -- When using the built-in select function, ftplugin settings fail to load
                     -- Issue does not occur if we use edit instead
                     select = function(list_item, list, options)
                         logger:log("config_default#select", list_item, list.name, options)
@@ -100,91 +98,31 @@ return {
                 harpoon.ui:toggle_quick_menu(harpoon:list(), { height_in_lines = 10 })
             end)
 
-            -- TODO: The way this is written is redundant
-            for i = 1, 9 do
-                vim.keymap.set("n", string.format("<leader>%s", i), function()
+            local mark = 10
+            for _ = 1, 10 do
+                -- Need to bring mark into this scope, or else final value of mark is
+                -- used for all maps
+                local this_mark = mark -- 10, 1, 2, 3, 4, 5, 6, 7, 8, 9
+                local mod_mark = this_mark % 10 -- 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+                vim.keymap.set("n", string.format("<leader>%s", mod_mark), function()
                     if vim.bo.filetype == "qf" then
                         print("Currently in quickfix list")
                         return
                     end
 
-                    harpoon:list():select(i)
+                    harpoon:list():select(this_mark)
                 end)
-            end
-            vim.keymap.set("n", string.format("<leader>%s", 0), function()
-                if vim.bo.filetype == "qf" then
-                    print("Currently in quickfix list")
-                    return
-                end
 
-                harpoon:list():select(10)
-            end)
+                mark = mod_mark + 1
+            end
 
             local harpoonEx = require("harpoonEx")
-            local extensions = require("harpoon.extensions")
             harpoon:extend(extensions.builtins.navigate_with_number())
             harpoon:extend(harpoonEx.extend())
 
             vim.keymap.set("n", "<leader>ar", function()
                 harpoonEx.delete(harpoon:list())
             end, { desc = "Delete current file from Harpoon List" })
-
-            -- require("harpoon-tabline").setup({
-            --     use_editor_color_scheme = false,
-            --     tab_prefix = "  ",
-            --     tab_suffix = "  ",
-            -- })
-
-            -- -- TODO: Should be a global
-            -- local c = {
-            --     fg = "#EFEFFD",
-            --     comment = "#3c778c",
-            --     yellow = "#EDFF98",
-            -- }
-            --
-            -- if Env_Theme == "blue" then
-            --     vim.api.nvim_set_hl(0, "HarpoonInactive", {
-            --         fg = c.fg,
-            --         bg = vim.api.nvim_get_hl(0, { name = "ColorColumn" }).bg,
-            --     })
-            --     vim.api.nvim_set_hl(0, "HarpoonNumberInactive", {
-            --         fg = c.yellow,
-            --         bg = vim.api.nvim_get_hl(0, { name = "ColorColumn" }).bg,
-            --     })
-            --     vim.api.nvim_set_hl(0, "HarpoonActive", {
-            --         fg = c.fg,
-            --         bg = c.comment,
-            --     })
-            --     vim.api.nvim_set_hl(0, "HarpoonNumberActive", {
-            --         fg = c.yellow,
-            --         bg = c.comment,
-            --     })
-            --     vim.api.nvim_set_hl(0, "TabLineFill", {
-            --         fg = vim.api.nvim_get_hl(0, { name = "CursorLineNr" }).fg,
-            --         bg = vim.api.nvim_get_hl(0, { name = "ColorColumn" }).bg,
-            --     })
-            -- else
-            --     vim.api.nvim_set_hl(0, "HarpoonInactive", {
-            --         fg = vim.api.nvim_get_hl(0, { name = "String" }).fg,
-            --         bg = vim.api.nvim_get_hl(0, { name = "ColorColumn" }).bg,
-            --     })
-            --     vim.api.nvim_set_hl(0, "HarpoonNumberInactive", {
-            --         fg = vim.api.nvim_get_hl(0, { name = "Type" }).fg,
-            --         bg = vim.api.nvim_get_hl(0, { name = "ColorColumn" }).bg,
-            --     })
-            --     vim.api.nvim_set_hl(0, "HarpoonActive", {
-            --         fg = vim.api.nvim_get_hl(0, { name = "String" }).fg,
-            --         bg = "#6A4C7F",
-            --     })
-            --     vim.api.nvim_set_hl(0, "HarpoonNumberActive", {
-            --         fg = vim.api.nvim_get_hl(0, { name = "Type" }).fg,
-            --         bg = "#6A4C7F",
-            --     })
-            --     vim.api.nvim_set_hl(0, "TabLineFill", {
-            --         fg = vim.api.nvim_get_hl(0, { name = "String" }).fg,
-            --         bg = vim.api.nvim_get_hl(0, { name = "ColorColumn" }).bg,
-            --     })
-            -- end
         end,
     },
 }
