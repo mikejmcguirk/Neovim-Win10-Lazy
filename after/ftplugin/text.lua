c7c53c9db9321bf3fb547f37a97598dbde05c7c9
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-02-18 16:27:33 -0500

    overwrote norm pastes in text files in ftplugin

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,3 +1,4 @@
+local ut = require("mjm.utils")
 local bp = require("mjm.backplacer")
 
 vim.opt_local.wrap = true
@@ -11,6 +12,39 @@ vim.keymap.set("i", ":", ":<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", "-", "-<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", "?", "?<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", "!", "!<C-g>u", { silent = true, buffer = true })
+
+local norm_pastes = {
+    { "p", "p", '"' },
+    { "<leader>p", '"+p', "+" },
+    { "P", "P", '"' },
+    { "<leader>P", '"+P', "+" },
+}
+-- TODO: Don't just repeat the code from keymap.lua
+-- Just repasted here for now because automated indenting in text files creates cooked results
+for _, map in pairs(norm_pastes) do
+    vim.keymap.set("n", map[1], function()
+        if not ut.check_modifiable() then
+            return
+        end
+
+        local cur_row, cur_col = unpack(vim.api.nvim_win_get_cursor(0))
+        vim.api.nvim_buf_set_mark(0, "z", cur_row, cur_col, {})
+
+        local status, result = pcall(function()
+            vim.api.nvim_exec2("silent norm! " .. vim.v.count1 .. map[2], {})
+        end) ---@type boolean, unknown|nil
+        if not status then
+            if type(result) == "string" then
+                vim.api.nvim_err_writeln(result)
+            else
+                vim.api.nvim_err_writeln("Unknown error when pasting")
+            end
+            return
+        end
+
+        vim.api.nvim_exec2("silent norm! `z", {})
+    end, { silent = true })
+end
 
 -- There should be something like gc where I can take a few lines and turn them into a
 -- bulleted list, either in normal or visual mode
