87c9270a75c88c1b1a193aca265c033f1789559c
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-03-13 01:07:47 -0400

    Added the worst map of all time in Go

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -5,3 +5,5 @@ vim.opt_local.expandtab = false
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 0
+
+vim.keymap.set("n", "--e", "oif err!= nil {<cr>}<esc>O//<esc>")
