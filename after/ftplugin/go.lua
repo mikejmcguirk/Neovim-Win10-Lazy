0249522d39219285402df46d32010cd1a075af77
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-03-12 22:58:50 -0400

    Setup Go to use tabs in ftplugin

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,7 @@
+vim.opt_local.listchars = { tab = "   ", extends = "»", precedes = "«", nbsp = "␣" }
+
+-- Since Go uses tabs
+vim.opt_local.expandtab = false
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 0
