882548e84cf66d780859ef68f7643a6f172fc6ed
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-03-15 22:33:12 -0400

    Commented out tmux stuff in zen vew since we're using Wezterm now

--- lua/mjm/plugins/zen.lua
+++ lua/mjm/plugins/zen.lua
@@ -1,6 +1,6 @@
 -- Anything tmux related is handled manually because, by default, when zen-mode closes, it also
 -- unhides all tmux panes
-local tmux_safety = vim.api.nvim_create_augroup("tmux_safety", { clear = true })
+-- local tmux_safety = vim.api.nvim_create_augroup("tmux_safety", { clear = true })
 
 return {
     "folke/zen-mode.nvim",
@@ -13,28 +13,30 @@ return {
                 showcmd = true,
             },
         },
+        -- No longer using tmux, but keeping the code here and commented out because it could be
+        -- useful in the future
         on_open = function()
             -- Just pcall anything tmux related so we don't have issues on non-tmux setups
-            pcall(function()
-                vim.fn.system([[tmux set status off]])
-            end)
+            -- pcall(function()
+            --     vim.fn.system([[tmux set status off]])
+            -- end)
 
             -- It's more logical in an abstract sense to clean when exiting Zen, but
             -- do it here to reduce the chance of a duplicate autocmd being created because
             -- of unforeseen behavior
-            vim.api.nvim_clear_autocmds({ group = "tmux_safety" })
+            -- vim.api.nvim_clear_autocmds({ group = "tmux_safety" })
             -- Needed because the on_close callback does not fire, at least not in the expected
             -- manner, when Nvim is closed from within a Zen window
-            vim.api.nvim_create_autocmd({ "VimLeave" }, {
-                group = tmux_safety,
-                once = true,
-                pattern = "*",
-                callback = function()
-                    pcall(function()
-                        vim.fn.system([[tmux set status on]])
-                    end)
-                end,
-            })
+            -- vim.api.nvim_create_autocmd({ "VimLeave" }, {
+            --     group = tmux_safety,
+            --     once = true,
+            --     pattern = "*",
+            --     callback = function()
+            --         pcall(function()
+            --             vim.fn.system([[tmux set status on]])
+            --         end)
+            --     end,
+            -- })
 
             -- Avoid issue where you toggle NvimTree closed underneath the Zen window
             vim.keymap.set("n", "<leader>nt", "<cmd>NvimTreeOpen<cr>")
@@ -45,31 +47,33 @@ return {
             vim.keymap.set("n", "<leader>nt", "<cmd>NvimTreeToggle<cr>")
             vim.keymap.set("n", "<leader>nf", "<cmd>NvimTreeFocus<cr>")
 
-            pcall(function()
-                vim.fn.system([[tmux set status on]])
-            end)
+            -- pcall(function()
+            --     vim.fn.system([[tmux set status on]])
+            -- end)
         end,
     },
     -- Done using an init because config overwrites the opts table
     init = function()
         vim.keymap.set("n", "<leader>e", function()
             local view = require("zen-mode.view")
-            if not view.is_open() then
-                local bad_filetypes = {
-                    "NvimTree",
-                    "harpoon",
-                }
-                for _, ft in pairs(bad_filetypes) do
-                    if vim.bo.filetype == ft then
-                        vim.notify("Zen open map disabled for filetype " .. ft)
-                        return
-                    end
-                end
 
-                view.open()
-            else
+            if view.is_open() then
                 view.close()
+                return
             end
+
+            local bad_filetypes = {
+                "NvimTree",
+                "harpoon",
+            }
+            for _, ft in pairs(bad_filetypes) do
+                if vim.bo.filetype == ft then
+                    vim.notify("Zen open map disabled for filetype " .. ft)
+                    return
+                end
+            end
+
+            view.open()
         end)
     end,
 }
