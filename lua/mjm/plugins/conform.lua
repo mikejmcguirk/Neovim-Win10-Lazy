d73306686201220935e10929385a1765ef9edfd5
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-12 14:55:23 -0400

    conform trimming

--- lua/mjm/plugins/conform.lua
+++ lua/mjm/plugins/conform.lua
@@ -1,5 +1,7 @@
 return {
     {
+        -- FUTURE: Figure out a way, in a Dadbod buffer, to determine what type of SQL server
+        -- we're connected to and then run the appropriate formatter over it
         "stevearc/conform.nvim",
         event = { "BufReadPre", "BufNewFile" },
         config = function()
@@ -10,17 +12,10 @@ return {
                     html = { "prettier" },
                     json = { "prettier" },
                     lua = { "stylua" },
-                    -- Markdown formatting does too many unpredictable things
-                    -- Maybe at some point in the future it can come back
-                    -- markdown = { "prettier" },
                     python = { "ruff_format" },
                     rust = { "rustfmt" },
                     sh = { "shfmt" },
-                    -- sh = { "beautysh" },
                     toml = { "taplo" },
-                    -- TODO: This is tough to swing with dadbod-ui because it stores queries in
-                    -- tmp or a local dir. You could edit the g variables or something but iunno
-                    -- sql = { "sqlfluff" },
                 },
             })
 
