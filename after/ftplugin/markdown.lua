e8a3d8b9cf1cfdedfec6a298dc0827855d82bd8e
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-12 21:20:53 -0400

    ftplugin touchup

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,11 +3,11 @@ vim.bo.tabstop = width
 vim.bo.softtabstop = width
 vim.bo.shiftwidth = width
 
-vim.opt_local.wrap = true
-vim.opt_local.spell = true
 vim.opt_local.colorcolumn = ""
-vim.opt_local.sidescrolloff = 12
 vim.opt_local.cursorlineopt = "screenline"
+vim.opt_local.wrap = true
+vim.opt_local.sidescrolloff = 12
+vim.opt_local.spell = true
 
 vim.keymap.set("i", ",", ",<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", ".", ".<C-g>u", { silent = true, buffer = true })
