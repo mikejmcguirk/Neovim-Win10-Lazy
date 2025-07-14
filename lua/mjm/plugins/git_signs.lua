d73f26985b02785222378bcf9f41e61f7eab0d86
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-14 01:03:04 -0400

    Updated Git Signs config to include all recommended maps

--- lua/mjm/plugins/git_signs.lua
+++ lua/mjm/plugins/git_signs.lua
@@ -9,31 +9,64 @@ return {
                     change = { text = "~" },
                 },
                 on_attach = function(bufnr)
-                    local gs = require("gitsigns")
+                    local gitsigns = require("gitsigns")
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
 
-                    vim.keymap.set("n", "]c", function()
+                    map("n", "]c", function()
                         if vim.wo.diff then
-                            vim.cmd("silent norm! ]c")
+                            vim.cmd.normal({ "]c", bang = true })
                         else
-                            gs.nav_hunk("next")
+                            gitsigns.nav_hunk("next")
                         end
-                    end, { silent = true })
+                    end)
 
-                    vim.keymap.set("n", "[c", function()
+                    map("n", "[c", function()
                         if vim.wo.diff then
-                            vim.cmd("silent norm! [c")
+                            vim.cmd.normal({ "[c", bang = true })
                         else
-                            gs.nav_hunk("prev")
+                            gitsigns.nav_hunk("prev")
                         end
-                    end, { silent = true })
+                    end)
 
-                    vim.keymap.set("n", "<leader>hd", gs.diffthis)
-                    vim.keymap.set("n", "<leader>hp", gs.preview_hunk)
-                    vim.keymap.set("n", "<leader>hr", gs.reset_hunk)
-                    vim.keymap.set("n", "<leader>hS", gs.stage_buffer)
-                    vim.keymap.set("n", "<leader>hi", gs.preview_hunk_inline)
+                    map("n", "<leader>hs", gitsigns.stage_hunk)
+                    map("n", "<leader>hr", gitsigns.reset_hunk)
 
-                    vim.keymap.set({ "o", "x" }, "ih", gs.select_hunk)
+                    map("v", "<leader>hs", function()
+                        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end)
+
+                    map("v", "<leader>hr", function()
+                        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end)
+
+                    map("n", "<leader>hS", gitsigns.stage_buffer)
+                    map("n", "<leader>hR", gitsigns.reset_buffer)
+                    map("n", "<leader>hp", gitsigns.preview_hunk)
+                    map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+                    map("n", "<leader>hb", function()
+                        gitsigns.blame_line({ full = true })
+                    end)
+
+                    map("n", "<leader>hd", gitsigns.diffthis)
+
+                    map("n", "<leader>hD", function()
+                        gitsigns.diffthis("~")
+                    end)
+
+                    map("n", "<leader>hQ", function()
+                        gitsigns.setqflist("all")
+                    end)
+                    map("n", "<leader>hq", gitsigns.setqflist)
+
+                    map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+                    map("n", "<leader>tw", gitsigns.toggle_word_diff)
+
+                    map({ "o", "x" }, "ih", gitsigns.select_hunk)
                 end,
             })
         end,
