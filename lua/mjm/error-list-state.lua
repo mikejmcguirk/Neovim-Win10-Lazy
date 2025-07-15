22348be94e288631bb1ae6c519c9c9e91f056647
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-15 11:11:01 -0400

    typo

--- lua/mjm/error-list-state.lua
+++ lua/mjm/error-list-state.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.last_grep = nil
 -- FUTURE: This could be tied to the window the last lgrep was run on as well, but that would
--- limit flexibility. Don't want to mak that change unless re-lgrepping the wrong window
+-- limit flexibility. Don't want to make that change unless re-lgrepping the wrong window
 -- turns out to be a footgun
 M.last_lgrep = nil
 
