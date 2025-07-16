48b6bd1223a15bbbcacd0509c5f2532937f0431b
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-16 03:09:42 -0400

    Set netrw remap to the very beginning

--- lua/mjm/lazy.lua
+++ lua/mjm/lazy.lua
@@ -8,6 +8,11 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 
+-- Lazy requirement
+vim.keymap.set({ "n", "x" }, "<Space>", "<Nop>")
+vim.g.mapleader = " "
+vim.g.maplocaleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
