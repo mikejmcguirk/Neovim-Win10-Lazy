d22a3f6dc4161c131834aae022ce477285869208
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-17 20:03:23 -0400

    Updated luaftplugin so that annotations set correctly right after ---

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,20 +3,27 @@ local ut = require("mjm.utils")
 ---@param annotation string
 ---@return nil
 local function add_annotation(annotation)
-    local row_1 = vim.api.nvim_win_get_cursor(0)[1] ---@type integer
-    local row_0 = row_1 - 1
+    local row_1, col_0 = unpack(vim.api.nvim_win_get_cursor(0)) ---@type integer, integer
+    local row_0 = row_1 - 1 ---@type integer
+    local col_1 = col_0 + 1 ---@type integer
     local line = vim.api.nvim_get_current_line()
     local line_len = #line ---@type integer
 
-    if line:match("^%s*$") then -- All whitespace
-        local indent = ut.get_indent(row_1) or 0 ---@type integer
-        local padding = string.rep(" ", indent) ---@type string
-        vim.api.nvim_buf_set_text(0, row_0, 0, row_0, 0, { padding .. annotation .. " " })
-    elseif line:match("%s$") then -- Non-whitespace with trailing whitespace
-        vim.api.nvim_buf_set_text(0, row_0, line_len, row_0, line_len, { annotation .. " " })
-    else -- Non-whitespace, needs trailing whitespace added
-        local new_text = " " .. annotation .. " " ---@type string
-        vim.api.nvim_buf_set_text(0, row_0, line_len, row_0, line_len, { new_text })
+    -- Right after three dashes
+    if col_1 >= 3 and line:sub(col_1 - 2, col_1) == "---" and annotation:sub(1, 3) == "---" then
+        local truncated = annotation:sub(4) .. " " ---@type string
+        vim.api.nvim_buf_set_text(0, row_0, col_1, row_0, col_1, { truncated })
+    else
+        if line:match("^%s*$") then -- All whitespace
+            local indent = ut.get_indent(row_1) or 0 ---@type integer
+            local padding = string.rep(" ", indent) ---@type string
+            vim.api.nvim_buf_set_text(0, row_0, 0, row_0, 0, { padding .. annotation .. " " })
+        elseif line:match("%s$") then -- Non-whitespace with trailing whitespace
+            vim.api.nvim_buf_set_text(0, row_0, line_len, row_0, line_len, { annotation .. " " })
+        else -- Non-whitespace, needs trailing whitespace added
+            local new_text = " " .. annotation .. " " ---@type string
+            vim.api.nvim_buf_set_text(0, row_0, line_len, row_0, line_len, { new_text })
+        end
     end
 
     vim.cmd("startinsert!")
@@ -40,6 +47,10 @@ end, { buffer = true })
 
 vim.keymap.set("n", "--d", function()
     add_annotation("---@diagnostic")
+end, { buffer = true })
+
+vim.keymap.set("n", "--c", function()
+    add_annotation("---@class")
 end, { buffer = true })
 
 vim.keymap.set("n", "--f", function()
