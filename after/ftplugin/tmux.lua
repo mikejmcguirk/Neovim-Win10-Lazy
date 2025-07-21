73c587e73d712fc9f890eee62e0b2dfed8aec487
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-07-21 08:48:42 -0400

    Updated tmux save autocmd with better pattern matching

--- after/ftplugin/tmux.lua
+++ after/ftplugin/tmux.lua
@@ -1,8 +1,9 @@
 local ut = require("mjm.utils")
 vim.api.nvim_create_autocmd("BufWritePre", {
     group = vim.api.nvim_create_augroup("tmux_save", { clear = true }),
-    pattern = "tmux", -- Since you can have .tmux and tmux.conf files
+    pattern = { "*.tmux", "tmux.conf" },
     callback = function(ev)
+        print(ev.match)
         if vim.api.nvim_get_option_value("filetype", { buf = ev.buf }) == "tmux" then
             ut.fallback_formatter(ev.buf)
         end
