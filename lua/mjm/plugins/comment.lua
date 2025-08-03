b41728dce4ddfd40d91efd35c0151dcf9883a501
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-08-03 13:44:52 -0400

    Added comment config to ignore blank lines

--- lua/mjm/plugins/comment.lua
+++ lua/mjm/plugins/comment.lua
@@ -2,6 +2,9 @@ vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
     group = vim.api.nvim_create_augroup("load-comment", { clear = true }),
     once = true,
     callback = function()
-        require("Comment").setup()
+        --- @diagnostic disable: missing-fields
+        require("Comment").setup({
+            ignore = "^$",
+        })
     end,
 })
