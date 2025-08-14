0765f3d24dd414a29f7ee51868572611b97fbb18
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-08-14 02:54:24 -0400

    Disable mouse completely

--- lua/mjm/set.lua
+++ lua/mjm/set.lua
@@ -1,5 +1,5 @@
 -- Otherwise, the terminal handles mouse functionality
-vim.api.nvim_set_option_value("mouse", "a", { scope = "global" })
+vim.api.nvim_set_option_value("mouse", "", { scope = "global" })
 vim.api.nvim_set_option_value("mousescroll", "ver:0,hor:0", { scope = "global" })
 
 -- On my monitors, for files under 10k lines, a centered vsplit will be on the color column
