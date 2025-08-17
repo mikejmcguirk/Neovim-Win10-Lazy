1d942156acdd93d7e2d579384567f889c95ce8ab
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-08-17 17:08:41 -0400

    Fixed omap for gg. Returning nop stays in omap. Returning esc exits it

--- lua/mjm/keymap.lua
+++ lua/mjm/keymap.lua
@@ -342,7 +342,8 @@ vim.keymap.set({ "n", "x" }, "j", function()
     end
 end, { expr = true, silent = true })
 
-vim.keymap.set({ "n", "x", "o" }, "gg", "<nop>")
+vim.keymap.set({ "n", "x" }, "gg", "<nop>")
+vim.keymap.set("o", "gg", "<esc>")
 vim.keymap.set({ "n", "x", "o" }, "go", function()
     if vim.v.count < 1 then
         return "gg" -- I have startofline off, so this keeps cursor position
