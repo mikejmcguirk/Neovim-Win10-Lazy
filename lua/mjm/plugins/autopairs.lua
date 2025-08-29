44c3fc824a241a85bf8341698e77befbeae2a239
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-08-29 03:12:01 -0400

    Update autopairs config to avoid ts queries

--- lua/mjm/plugins/autopairs.lua
+++ lua/mjm/plugins/autopairs.lua
@@ -2,11 +2,11 @@ local function setup_autopairs()
     local npairs = require("nvim-autopairs")
     local rule = require("nvim-autopairs.rule")
     local cond = require("nvim-autopairs.conds")
-    local ts_conds = require("nvim-autopairs.ts-conds")
+    -- local ts_conds = require("nvim-autopairs.ts-conds")
 
-    npairs.setup({
-        check_ts = true,
-    })
+    -- npairs.setup({
+    --     check_ts = true,
+    -- })
 
     -- Autoclosing angle-brackets for generics
     -- NOTE: Just spot checking, this seems to adequately handle the main cases this
@@ -21,15 +21,15 @@ local function setup_autopairs()
     end))
 
     -- Auto add commas after lua table entries
-    npairs.add_rules({
-        rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-        -- Triggers on comments after constructor lines
-        -- rule("'", "',", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-        -- rule('"', '",', "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-    })
+    -- npairs.add_rules({
+    -- rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+    -- Triggers on comments after constructor lines
+    -- rule("'", "',", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+    -- rule('"', '",', "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+    -- })
 end
 
-vim.api.nvim_create_autocmd("InsertEnter", {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPre" }, {
     group = vim.api.nvim_create_augroup("setup-autopairs", { clear = true }),
     once = true,
     callback = function()
