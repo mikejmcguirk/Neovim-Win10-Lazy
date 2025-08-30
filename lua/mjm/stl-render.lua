e60ae1775c93d53ad41276133b52fa2a82c0dd48
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-08-30 02:54:43 -0400

    Use g:gitsigns head for stl

--- lua/mjm/stl-render.lua
+++ lua/mjm/stl-render.lua
@@ -26,7 +26,7 @@ local format_icons = { unix = "unix", dos = "dos", mac = "mac" }
 local function build_active_a(stl)
     -- TODO: Use gitsigns_status_dict
     -- table.insert(stl, " %#stl_a#%{FugitiveStatusline()} %{get(b:,'gitsigns_status','')}%*")
-    table.insert(stl, " %#stl_a#%{get(b:,'gitsigns_head','')} %{get(b:,'gitsigns_status','')}%*")
+    table.insert(stl, " %#stl_a#%{get(g:,'gitsigns_head','')} %{get(b:,'gitsigns_status','')}%*")
 end
 
 -- TODO: How to only add spacing for the %m option if it displays
