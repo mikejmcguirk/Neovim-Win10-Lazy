2c4179b384a91c7f82e57533edfc8782cbe84fcc
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-08-30 22:47:53 -0400

    Re-create TSEditQueryFile

--- lua/mjm/treesitter.lua
+++ lua/mjm/treesitter.lua
@@ -1,13 +1,4 @@
--- TODO: Implement Helix style Treesitter navigation. We would want
--- - vie - Select current node
--- - vae - Select parent node
--- - [e and ]e navigate to next sibling (with wrap)
--- - In visual, would add to select I think, but check Text Objects
--- - A selection for next parent or previous parent
--- - I think with ie, to start it would pull the lowest level node at the cursor, then ae would
--- go up a level and ie would do down a level, like how an/in work
--- https://github.com/drybalka/tree-climber.nvim
--- https://github.com/David-Kunz/treesitter-unit
+local ut = require("mjm.utils")
 
 vim.keymap.set("n", "gtt", function()
     if vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] then
@@ -25,44 +16,42 @@ vim.keymap.set("n", "gtee", function()
     vim.api.nvim_cmd({ cmd = "EditQuery" }, {})
 end)
 
--- TODO: Re-create these commands from the Treesitter master branch
+---@param query_group string
+---Lifted from the old TS Master Branch
+local function edit_query_file(query_group)
+    local lang = vim.api.nvim_get_option_value("filetype", { buf = 0 })
+    local files = vim.treesitter.query.get_files(lang, query_group, nil)
 
--- vim.keymap.set("n", "gtei", function()
---     if #vim.api.nvim_tabpage_list_wins(0) == 1 then
---         --- @diagnostic disable: missing-fields
---         vim.api.nvim_cmd({ cmd = "vsplit", mods = { split = "botright" } }, {})
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "highlights" } }, {})
---     else
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "highlights" } }, {})
---     end
--- end)
---
--- vim.keymap.set("n", "gten", function()
---     if #vim.api.nvim_tabpage_list_wins(0) == 1 then
---         --- @diagnostic disable: missing-fields
---         vim.api.nvim_cmd({ cmd = "vsplit", mods = { split = "botright" } }, {})
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "indents" } }, {})
---     else
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "indents" } }, {})
---     end
--- end)
---
--- vim.keymap.set("n", "gtej", function()
---     if #vim.api.nvim_tabpage_list_wins(0) == 1 then
---         --- @diagnostic disable: missing-fields
---         vim.api.nvim_cmd({ cmd = "vsplit", mods = { split = "botright" } }, {})
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "injections" } }, {})
---     else
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "injections" } }, {})
---     end
--- end)
---
--- vim.keymap.set("n", "gteo", function()
---     if #vim.api.nvim_tabpage_list_wins(0) == 1 then
---         --- @diagnostic disable: missing-fields
---         vim.api.nvim_cmd({ cmd = "vsplit", mods = { split = "botright" } }, {})
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "folds" } }, {})
---     else
---         vim.api.nvim_cmd({ cmd = "TSEditQuery", args = { "folds" } }, {})
---     end
--- end)
+    if #files == 0 then
+        vim.api.nvim_echo({ { "No query file found", "" } }, false, {})
+        return
+    elseif #files == 1 then
+        ut.open_buf(files[1], { open = "vsplit" })
+    else
+        vim.ui.select(files, { prompt = "Select a file:" }, function(file)
+            if file then
+                ut.open_buf(file, { open = "vsplit" })
+            end
+        end)
+    end
+end
+
+vim.keymap.set("n", "gteo", function()
+    edit_query_file("folds")
+end)
+
+vim.keymap.set("n", "gtei", function()
+    edit_query_file("highlights")
+end)
+
+vim.keymap.set("n", "gten", function()
+    edit_query_file("indents")
+end)
+
+vim.keymap.set("n", "gtej", function()
+    edit_query_file("injections")
+end)
+
+vim.keymap.set("n", "gtex", function()
+    edit_query_file("textobjects")
+end)
