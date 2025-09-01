c82fa9db1f4a8788bf993d19fa2bcebfef01a1c0
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-09-01 15:27:56 -0400

    Creat keymap global variable

--- lua/mjm/global_vars.lua
+++ lua/mjm/global_vars.lua
@@ -3,3 +3,7 @@ Highlight_Time = 175 --- @type integer
 Has_Nerd_Font = true --- @type boolean
 Scrolloff_Val = 6 ---@type integer
 SpellFile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add" ---@type string
+
+Map = function(m, lhs, rhs, opts)
+    vim.keymap.set(m, lhs, rhs, opts)
+end
