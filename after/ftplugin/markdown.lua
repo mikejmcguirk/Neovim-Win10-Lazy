9b7cd53317f595bd406284776e25449a62b4949c
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-09-01 08:25:34 -0400

    Move Markdown formatting control to ftplugin file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -9,6 +9,9 @@ vim.opt_local.wrap = true
 vim.opt_local.sidescrolloff = 12
 vim.opt_local.spell = true
 
+-- "r" in Markdown treats lines like "- some text" as comments and indents them
+vim.opt.formatoptions:append("r")
+
 vim.keymap.set("i", ",", ",<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", ".", ".<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", ":", ":<C-g>u", { silent = true, buffer = true })
@@ -16,12 +19,12 @@ vim.keymap.set("i", "-", "-<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", "?", "?<C-g>u", { silent = true, buffer = true })
 vim.keymap.set("i", "!", "!<C-g>u", { silent = true, buffer = true })
 
-local ut = require("mjm.utils")
-vim.keymap.set("n", "K", ut.check_word_under_cursor)
+vim.keymap.set("n", "K", require("mjm.utils").check_word_under_cursor)
+
 vim.api.nvim_create_autocmd("BufWritePre", {
     group = vim.api.nvim_create_augroup("md_save", { clear = true }),
     pattern = "*.md",
     callback = function(ev)
-        ut.fallback_formatter(ev.buf)
+        require("mjm.utils").fallback_formatter(ev.buf)
     end,
 })
