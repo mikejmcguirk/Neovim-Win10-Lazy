a75d0071ce970c6e4df01aa2f104748c7eea5be1
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-09-05 23:39:30 -0400

    Use sticky cursor in nvim-surround

--- lua/mjm/plugins/nvim-surround.lua
+++ lua/mjm/plugins/nvim-surround.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
     group = vim.api.nvim_create_augroup("load-nvim-surround", { clear = true }),
     once = true,
     callback = function()
-        require("nvim-surround").setup({})
+        require("nvim-surround").setup({ move_cursor = "sticky" })
         vim.api.nvim_del_augroup_by_name("load-nvim-surround")
     end,
 })
