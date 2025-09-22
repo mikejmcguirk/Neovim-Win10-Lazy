0131afb0b98d71714b0ece8cba0330bfbbdc9bbc
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-09-22 01:19:38 -0400

    Put all protected win close logic into one function

--- lua/mjm/error-list-open.lua
+++ lua/mjm/error-list-open.lua
@@ -1,65 +1,79 @@
 local M = {}
 
---- @param win integer
+--- @class QfRancherPWinCloseOpts
+--- @field bdel? boolean
+--- @field bwipeout? boolean
+--- @field force? boolean
+--- @field print_errors? boolean
+--- @field win? integer
+
+--- @param opts QfRancherPWinCloseOpts
 --- @return boolean, [string, string]|nil
-local function protected_win_close(win)
+--- Checks that the provided window is valid. If the provided window is the last one, deletes the
+--- buffer instead
+--- Opts:
+--- - buf_delete: (default false) Delist the buffer in addition to unloading it
+--- - buf_wipeout: (default false) Perform bwipeout on a deleted buffer. Overrides buf_delete
+--- - force: (default false) Ignore unsaved changes
+--- - print_errors: (default true) Print error messages
+--- - win: (default current win) The window to close
+local function pwin_close(opts)
+    opts = opts or {}
+    local win = opts.win or vim.api.nvim_get_current_win() --- @type integer
+    vim.validate("opts.win", opts.win, "number")
     if not vim.api.nvim_win_is_valid(win) then
-        return false, { "Window " .. win .. " is invalid", "WarningMsg" }
-    end
-
-    local tabpages = vim.api.nvim_list_tabpages()
-    local win_tabpage = vim.api.nvim_win_get_tabpage(win)
-    local win_tabpage_wins = vim.api.nvim_tabpage_list_wins(win_tabpage)
-    if #tabpages == 1 and #win_tabpage_wins == 1 then
-        return false, { "Cannot close the last window", "" }
-    end
-
-    local ok, err = pcall(vim.api.nvim_win_close, win, true)
-    if not ok then
-        local msg = err or ("Unknown error closing window " .. win)
-        return false, { msg, "ErrorMsg" }
-    end
-
-    return true
-end
-
---- @param win integer
---- @return boolean
-local function close_list_win(win, print_errors)
-    local ok, err = protected_win_close(win) --- @type boolean, [string, string]|nil
-    if ok then
-        return true
-    end
-
-    if err and err[1] == "Cannot close the last window" then
-        local buf = vim.api.nvim_win_get_buf(win) --- @type integer
-        if not vim.api.nvim_buf_is_valid(buf) then
-            local msg = "Bufnr " .. buf .. " in window " .. win .. " is not valid" --- @type string
-            vim.api.nvim_echo({ { msg, "ErrorMsg" } }, true, { err = true })
-            return false
+        local chunk = { "Window " .. win .. " is invalid", "WarningMsg" }
+        if opts.print_errors then
+            vim.api.nvim_echo({ chunk }, true, { err = true })
         end
 
-        vim.api.nvim_buf_delete(buf, { force = true })
-        return true
+        return false, chunk
     end
 
-    if not print_errors then
-        return false
+    local force = opts.force and true or false --- @type boolean
+    local tabpages = vim.api.nvim_list_tabpages() --- @type integer[]
+    local win_tabpage = vim.api.nvim_win_get_tabpage(win) --- @type integer
+    local win_tabpage_wins = vim.api.nvim_tabpage_list_wins(win_tabpage) --- @type integer[]
+    if #tabpages > 1 or #win_tabpage_wins > 1 then
+        local ok, err = pcall(vim.api.nvim_win_close, win, force) --- @type boolean, any
+        if not ok then
+            local msg = err or ("Unknown error closing window " .. win) --- @type string
+            local chunk = { msg, "ErrorMsg" } --- @type [string, string]
+            if opts.print_errors then
+                vim.api.nvim_echo({ chunk }, true, { err = true })
+            end
+
+            return false, { msg, "ErrorMsg" }
+        end
+
+        return true, nil
     end
 
-    --- @type string
-    local msg = (err and err[1]) and err[1] or "Unknown error in protected_win_close"
-    local hl = (err and err[2]) and err[2] or "ErrorMsg" --- @type string
-    vim.api.nvim_echo({ { msg, hl } }, true, { err = true })
-    return false
+    local buf = vim.api.nvim_win_get_buf(win) --- @type integer
+    if not vim.api.nvim_buf_is_valid(buf) then
+        local msg = "Bufnr " .. buf .. " in window " .. win .. " is not valid" --- @type string
+        local chunk = { msg, "ErrorMsg" } --- @type [string, string]
+        if opts.print_errors then
+            vim.api.nvim_echo({ chunk }, true, { err = true })
+        end
+
+        return false, chunk
+    end
+
+    local buf_delete_opts = opts.bwipeout and { force = force } or { force = force, unload = true }
+    if opts.bdel and not opts.bwipeout then
+        vim.api.nvim_set_option_value("buflisted", false, { buf = buf })
+    end
+
+    vim.api.nvim_buf_delete(buf, buf_delete_opts)
+    return true, nil
 end
 
 --- @param wins integer[]
---- @param print_errors boolean
 --- @return nil
-local function close_list_wins(wins, print_errors)
+local function close_list_wins(wins)
     for _, win in wins do
-        close_list_win(win, print_errors)
+        pwin_close({ bwipeout = true, force = true, win = win })
     end
 end
 
@@ -97,7 +111,7 @@ local max_qf_height = 10
 --- @return integer
 local function get_list_height(win, is_ll)
     vim.validate("win", win, "number")
-    local getlist = require("mjm.error-list-util").get_getlist(win, is_ll or false)
+    local getlist = require("mjm.error-list-util").get_getlist({ win = win, get_loclist = is_ll })
     local cur_size = getlist({ size = true }).size
 
     local list_height = math.min(cur_size, max_qf_height)
@@ -227,7 +241,7 @@ function M.close_qflist()
     end
 
     local views = get_views(wins) --- @type vim.fn.winsaveview.ret[]
-    close_list_win(qf_win, true)
+    pwin_close({ bwipeout = true, force = true, win = qf_win })
     restore_views(views)
     return true
 end
@@ -279,7 +293,7 @@ function M.open_loclist(opts)
     end
 
     if qf_win then
-        close_list_win(qf_win)
+        pwin_close({ bwipeout = true, force = true, win = qf_win })
     end
 
     local height = get_list_height(cur_win, false) --- @type integer
