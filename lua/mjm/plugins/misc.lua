d8e22a4c0d643158f0e98abe691a3d04ac818523
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-10-09 01:52:33 -0400

    Add misc plugins file

---
+++ lua/mjm/plugins/misc.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_set_var("db_ui_use_nerd_fonts", 1)
+
+vim.api.nvim_set_var("qs_highlight_on_keys", { "f", "F", "t", "T" })
+vim.api.nvim_set_var("qs_max_chars", 9999)
+vim.api.nvim_set_hl(0, "QuickScopePrimary", { reverse = true })
+vim.api.nvim_set_hl(0, "QuickScopeSecondary", { undercurl = true })
+
+Map("n", "<leader>u", function()
+    local win_width = vim.api.nvim_win_get_width(vim.api.nvim_get_current_win()) --- @type integer
+    local open_width = math.floor(win_width * 0.3) --- @type integer
+    open_width = math.max(open_width, 30) --- @type integer
+    local command = open_width .. "vnew" --- @type string
+
+    ---@diagnostic disable-next-line: missing-fields
+    require("undotree").open({ command = command })
+end)
