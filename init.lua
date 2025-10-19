347efc1376db4350b5286003962dc2b1586e9b9d
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-10-19 02:44:25 -0400

    commentary

--- init.lua
+++ init.lua
@@ -1,37 +1,28 @@
 require("mjm")
 
--- TODO: Move the notes back into the files. Was bad, friction creating idea
 -- TODO: https://github.com/neovim/neovim/issues/36081 - I might be able to get this
 -- TODO: Fix the missing fields annotations in certain keysets (nvim_cmd being the worst)
 
------------
---- MID ---
------------
+-- MID: https://github.com/neovim/neovim/pull/35448 -- Could be easier way to handle this
 
--- https://github.com/neovim/neovim/pull/35448 -- Could be easier way to handle this
+-- LOW: How does Fugitive do line wrap in commit buffers?
 
------------
---- LOW ---
------------
-
---- How does Fugitive do line wrap in commit buffers?
-
-------------------------
---- DOTFILE STALKING ---
-------------------------
+----------------------
+-- DOTFILE STALKING --
+----------------------
 
 -- https://github.com/folke/dot/tree/master/nvim
 -- https://github.com/stevearc/dotfiles/tree/master/.config/nvim
 -- https://github.com/b0o/nvim-conf/tree/main
 
------------------
---- RESOURCES ---
------------------
+---------------
+-- RESOURCES --
+---------------
 
 -- https://github.com/tjdevries/lazy-require.nvim
 
--------------------
---- OTHER NOTES ---
--------------------
+-----------------
+-- OTHER NOTES --
+-----------------
 
 -- export NVIM_APPNAME=some-other-thing will make the config .config/some-other-thing
