444aa358b9be4fb2e090b9bab1492ebedf01634a
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-10-21 15:11:47 -0400

    style(entry): clean up todolist in qfr mainf ile

--- lua/mjm/error-list.lua
+++ lua/mjm/error-list.lua
@@ -124,187 +124,88 @@ if vim.g.qfr_create_autocmds then
     })
 end
 
+---@export NvimQfRancher
+
 -- NOTES:
 -- - Any space breaks in annotation comments are intentional to make lemmy-ehlp ignore them
 
----@export NvimQfRancher
-
--- TODO: Create docgen script. 'lemmy-help -l "compact" fnames > output.txt'
+-- TODO: Create docgen script. 'lemmy-help -l "compact" [fnames] > output.txt'
 -- TODO: The project's luarc should contain the trailing whitespace nop
 
 -- DOCUMENT: For cmd mappings, document what cmd opts they expect to be available
 
--- Doc sanitation. Remove unnecessary doc strings
--- For anything with one reference, merge it in
--- Use the command mentality throughout
--- Formatting: api/fn/other long names. defer require
--- For validation and such, the g variables should be as generous as possible with user error,
---     the actual API stuff should be strict
--- For organizing the module opts, the module specific input opts should be tied specifically to
---     the command. This is your conceptual anchor. With that, I think we can consider the
---     APIs and such more baked in
--- Put a confirm statement in every project file. It should be possible to open Nvim and then
--- a list without eager requiring other modules
--- Function audit - Is the win variable a src_win, list_win, something else? Don't just pass
---     "win" as a variable, as it is not necessarily clear what the win is supposed to be
--- Go through everything and be strict on data validation, including stuff that comes from the
---     user. We should only handle bad results from exterior programs
--- Cmd prefix customization
--- Orphan loclist handling:
--- - Grouping arbitrarily with other commands produces confusing behavior
--- - The autocmd to close associated loclists should be enabled by default
--- - Any close cmd needs to check if the window type is loclist so it can run in orphans
--- - The lE purge needs to properly handle orphans
--- - The autocmd to prevent loclists from moving should be enabled by default
--- Check validations for what should and should not be behind g vars. General rule - If it's
---     a public API or right behind one, should be validated. Same with functions that can cause
---     real issues if they go wrong (list cycling is the most obvious example)
+-- TEST: Put a confirm statement in every file. None of them should fire on startup
+-- Smarter way would be to inspect the cache of loaded modules
 
--- - I have filter and sort right now set to use new list as their default, in line with how
--- Cfilter works. And that should actually be the default. But I am thinking there might need
--- to be a way to not do that. Having to do qGf to sort by filename without pollutting the
--- stack seems cumbersome. And I don't want to just say "use the plug mappings" because there are
--- over 300 keymaps. It's not feasible
---
--- - For the keymaps, there has to be a way to edit how they're set from g vars. There are too
--- many maps to rely on plugs
--- - How to handle deferred maps, like for bufgrep and such
--- - Need some kind of convenience function for reading g variables that accounts for nil and
--- vim.NIL values, because I'm not sure that vim.NIL is falsy the same way
---
--- Putting list_item_type in output opts is correct because it has to do with the rendering of
--- of the list items. It also makes sense, in the main validation step, to allow nil values
--- through because individual functions might want to treat a nil list item type as something to
--- ignore. I guess the broader thing to just keep in mind is that the field can't be a gotcha
--- in contexts where you don't expect it to come up
---
--- Check the file for any vim.fn.confirm
--- Check that no eager requires happen on startup
--- DOuble check that all view saving sufficiently respects splitkeep/g:vars
--- Make sure g variables are doing what they should be
--- Audit everything for handling of 0 location lists. These are still-open loclist windows where
---     the stack has been wiped. The wintype will still be "loclist"
-
-----------
--- MID ---
-----------
-
--- Unsure how to deal with wintype annotations. Ideally they would be aliased in the core
---     codebase. Unsure if I should make my own alias here or not
--- Look into running more functionality through the default Qf commands given that they're tied
---     to the QuickFixCmd autocmds
--- Also look into what events we can use those autocmds to drive
--- Make resizing configurable
--- A problem with cdo is, say you make changes on a couple lines, then realizes you're better off
---     just using cdo to run a substitute command, you will get enter errors on the entries you
---     already changed. It would be better if cdo/cfdo ran in protected mode, perhaps showing
---     the errors afterwards. Unsure though if there's a way to properly capture context so
---     you can run each command pcalled individually, or if you have to do a nvim_cmd cdo and
---     run that whole thing in the pcall
--- Publish Qf items as diagnostics. Would make other ideas more useful
--- Commands to handle:
+-- MID: Alias wintype annotations?
+-- MID: Publish Qf items as diagnostics. Would make other ideas more useful
+-- MID: Remaining Commands to handle:
 -- - cexpr
 -- - cbuffer/cgetbuffer/caddbuffer
 -- - cfile
 -- - clist
 -- - cabove/cbelow
---     - It's tough to make these helpful without some sort of visual feedback, implying that you
---     would need a way to push qf entries to virtual text extmarks so they could be navigated.
---     This risks tripping over LSP diagnostics. Nvim alreaddy has diagnostic navigation
---     features built in, and this would be a tough feature to implement for an uncertain
---     use case
--- How to make the list more useful with compilers. Possible starting points:
+-- MID: How to make the list more useful with compilers. Possible starting points:
 -- - https://github.com/Zeioth/compiler.nvim
 -- - https://github.com/ahmedkhalf/project.nvim
 -- - https://github.com/stevearc/overseer.nvim
 -- - :h :make_makeprg
 -- - :h compiler-select
--- The open mappings and such should work in visual mode
--- Make a way to explicitly copy/replace/merge whole lists. This can be hacked with fitler, but
---     that means guaranteeting how an empty pattern behaves, which seems like an unnecessary
---     limitation
--- Look into the idea of brdiging lists between the loclist and qflists
--- Take all Qf entries and send their ranges to a normal buffer
--- - Could also send greps to normal buffers
---     - Implies a broader idea of what greps can do outside the error lists
--- - Could also writes bufs to the qflist
--- https://github.com/gabrielpoca/replacer.nvim - For edits and bulk changes, this seems to be
---     the way, where you put the Qflist into edit mode, and then re-integrate the changes
---     in a save step
---     https://github.com/stefandtw/quickfix-reflector.vim - A similar take
 -- MID: Add ftplugin files that use rancher, such as make commands piped to the system module
+-- MID: The open mappings and such should work in visual mode
+-- LOW: If we explore the idea of editing the qf buffer, the best way to do it seems to be to
+-- treat "edit mode" as a distinct thing, where it can then be saved and propagate the changes
+-- - https://github.com/gabrielpoca/replacer.nvim
+-- - https://github.com/stefandtw/quickfix-reflector.vim
 
-----------
--- LOW ---
-----------
-
+-- LOW: A way to copy/replace/merge whole lists
+-- LOW: Is there a way to bridge lists between the qf and loclists?
 -- LOW: View adjustments should take into account scrolloff and screenlines so that if the
 -- user re-enters the window, it doesn't shift to meet scrolloff requirements
+-- LOW: How to improve on cdo/cfdo? Random errors on substitution are bad
+-- cfdo is fairly feasible because you can win_call or buf_call on every file behind a pcall
+-- But then how to show errors
+-- LOW: Smoother way to run cmds from visual mode without manually removing the marks. I don't
+-- want the cmds to accept then throw away a range. Deceptive UI
+-- LOW: Better error format. The default masking of certain error types hides info from the
+-- user. Would also be helpful if pipe cols were more consistent
+-- LOW: ts-context integration in preview wins
+-- LOW: scrolling in preview wins
+-- LOW: Allow customizing windows to skip when looking for open targets:
+-- - https://github.com/kevinhwang91/nvim-bqf/issues/78
+-- LOW: Incremental preview of cdo/cfdo changes
+-- LOW: General cmd parsing: https://github.com/niuiic/quickfix.nvim
+-- LOW: Somehow auto-generate the keymaps. Would help with docgen
+-- LOW: Use a g:var to control regex case sensitivity
 
--- Something awkward is that, because marks are not supported, you cannot run any cmds from
---     visual mode without manually removing the marks with <C-u>. This adds friction to running
---     greps and filters from the cmd line in visual mode. You could bandaid this by allowing the
---     cmd to accept marks but then just ignoring them, but I think that then is deceptive toward
---     the user. The vim normal mode command just falls through. But since the greps can
---     work from visual marks, that feels more deceptive
--- Operations that move loclists over to qflists and vice-versa
--- Better error format? The masking of errors in particular is annoying
--- Show ts-context info in preview wins
--- Allow customizing windows to skip when looking for open targets:
---     https://github.com/kevinhwang91/nvim-bqf/issues/78
--- Test with the old nvim-treesitter master branch
--- Incremental preview of cdo/cfdo changes
--- A better way of handling cdo/cfo so you don't get spammed with enter errors
--- General cmd parsing: https://github.com/niuiic/quickfix.nvim
--- If 1k keymaps, autogen a DoD setup. Would actually save time
--- Use a g:var to control regex case sensitivity
--- Test load behavior against mksession
-
----------------
--- DOCUMENT ---
----------------
-
--- When writing the plugin description, don't focus on overly pastoral ranching imagery, as the
--- poitn of the Quickfix list is to be quick
--- The new/add/replace behavior
--- How smartcase works by default (the "vimcase" thing). Does it make sense?
---     Have to document this in the more general sense with "smartcase" and "insensitive" also as
---     options
--- Use of marks is not supported in cmds because they are row only
--- In the commands, a count of zero is treated as a no count
+-- DOCUMENT: vim.regex currently uses case sensitive default behavior
+-- DOCUMENT: cmds are not designed to be run in visual mode
+-- DOCUMENT: How default counts are treated in cmds and maps
 -- DOCUMENT: Buf greps use external grep
 -- DOCUMENT: qf Buf Grep is all bufs, ll Buf Grep is current buf only
 -- DOCUMENT: rg handles true multi-line greps. For programs that don't, or is used as a fallback
 -- DOCUMENT: The following are non-goals:
 -- - Creating persistent state beyond what is necessary to make the preview win work
--- - Modifying buffers within the qflist
+-- - Dynamically modifying buffers within the qflist
 -- - Providing additional context within the list itself. Covered by the preview win
 -- - No Fuzzy finding type stuff. FzfLua does this. And as far as I know, all the major finders
---     have the ability to search the qflists
+--   have the ability to search the qflists
 -- - No annotations. Should be able to filter down to key items
 -- - Dynamic behavior. Trouble has to create a whole async runtime and data model to manage this
 -- - "Modernizing" the feel of the qflist. The old school feel is part of the charm
--- Cmds don't accept ranges
--- The open functions double as resizers, as per the default cmd behavior
--- cn/cpfile does not have the same level of wrapping logic as cc
--- underline functions are not supported
--- What types of regex are used where. Grep cmds have their own regex. Regex filters use
---     vim regex
--- Note that vim regex is case sensitive by default(right?)
+-- DOCUMENT: Cmds don't accept ranges
+-- DOCUMENT: The open functions double as resizers, as per the default cmd behavior
+-- DOCUMENT: If open is run and the list is open, go to the list
+-- DOCUMENT: underline functions are not supported
+-- DOCUMENT: What types of regex are used where. Grep cmds have their own regex. Regex filters use
+-- vim regex
+-- DOCUMENT: The README should include alternatives, including quicker
 
------------
--- PR ---
------------
+-- PR: Fix wintype annotations
+-- PR: It should be possible to output vimgrep to a list so it can be used by internal scripting
+-- PR: It would be better if cmd marks produced rows and columns
 
--- It would be better if cmd marks produced rows and columns
--- Wintype is currently just an or enum. Should be an alias so it can be annotated
--- It should be possible to output vimgrep to a list so it can be used by internal scripting
-
----------------
 -- RESOURCES --
----------------
-
--- RESOURCES
 -- https://github.com/romainl/vim-qf
 -- https://github.com/kevinhwang91/nvim-bqf
 -- https://github.com/arsham/listish.nvim
@@ -315,6 +216,6 @@ end
 -- https://github.com/mhinz/vim-grepper
 -- https://github.com/ten3roberts/qf.nvim
 
--- PREVIEWERS:
+-- PREVIEWERS --
 -- https://github.com/r0nsha/qfpreview.nvim
 -- https://github.com/bfrg/vim-qf-preview
