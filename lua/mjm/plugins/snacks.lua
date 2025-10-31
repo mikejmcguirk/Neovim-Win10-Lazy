3b5194d58452f01a70301b02e2551fdb3e812b58
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-10-31 00:14:32 -0400

    trimming

--- lua/mjm/plugins/snacks.lua
+++ lua/mjm/plugins/snacks.lua
@@ -1,7 +1,13 @@
 local api = vim.api
+local group = "zen-tmux-status" ---@type string
+local tmux_status = vim.api.nvim_create_augroup(group, {}) ---@type integer
 
-local group_name = "zen-tmux-safety" ---@type string
-local tmux_safety = vim.api.nvim_create_augroup(group_name, {}) ---@type integer
+---@param cmd_parts string[]
+---@return nil
+local function if_tmux(cmd_parts)
+    if os.getenv("TMUX") == nil then return end
+    vim.system(cmd_parts, { text = true, timeout = 1000 })
+end
 
 require("snacks").setup({
     bigfile = { enabled = false },
@@ -20,41 +26,30 @@ require("snacks").setup({
         toggles = { dim = false, git_signs = false, mini_diff_signs = false },
         win = { width = 106, style = "zen" },
         on_open = function()
-            pcall(function()
-                vim.fn.system([[tmux set status off]])
-            end)
-
-            vim.api.nvim_clear_autocmds({ group = group_name })
+            if_tmux({ "tmux", "set", "status", "off" })
+            vim.api.nvim_clear_autocmds({ group = group })
             -- Don't rely on the on_close callback to re-open the tmux statusline
             vim.api.nvim_create_autocmd({ "VimLeave" }, {
-                group = tmux_safety,
-                once = true,
-                pattern = "*",
+                group = tmux_status,
                 callback = function()
-                    pcall(function()
-                        vim.fn.system([[tmux set status on]])
-                    end)
+                    if_tmux({ "tmux", "set", "status", "on" })
                 end,
             })
         end,
         on_close = function()
-            pcall(function()
-                vim.fn.system([[tmux set status on]])
-            end)
-
-            vim.api.nvim_clear_autocmds({ group = group_name })
+            if_tmux({ "tmux", "set", "status", "on" })
+            vim.api.nvim_clear_autocmds({ group = group })
         end,
     },
 })
 
-local bad_filetypes = { "NvimTree", "harpoon", "qf" }
 vim.keymap.set("n", "<leader>e", function()
     local zen = require("snacks.zen")
     if not zen.win then
         local buf = vim.api.nvim_get_current_buf() ---@type integer
         local ft = api.nvim_get_option_value("filetype", { buf = buf }) ---@type string
-        for _, bft in pairs(bad_filetypes) do
-            if ft == bft then
+        for _, bad_ft in pairs({ "NvimTree", "harpoon", "qf" }) do
+            if ft == bad_ft then
                 api.nvim_echo({ { "Zen disabled for filetype " .. ft } }, false, {})
                 return
             end
