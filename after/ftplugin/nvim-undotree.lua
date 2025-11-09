a87295880a9b805fed527e5ec49d883225d2e779
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-11-09 14:42:24 -0500

    feat(undotree): auto-center view change jumping entries. other cleanup

--- after/ftplugin/nvim-undotree.lua
+++ after/ftplugin/nvim-undotree.lua
@@ -1,1 +1,24 @@
-vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = true })
+local api = vim.api
+local fn = vim.fn
+
+vim.keymap.set("n", "q", "<cmd>lua require('undotree').open()<cr>", { buffer = true })
+-- PR: The undotree plugin should provide a hook to do this. Do so in the open opts I think
+-- PR: Slightly better would be - The module should know the buf the tree is attached to, and we
+-- should be able to use win_findbuf() rather than a bespoke iteration
+-- PR: Navigating the tree adds a lot of entries to the undolist. Possible way to mitigate?
+api.nvim_create_autocmd("CursorMoved", {
+    buffer = 0,
+    callback = function()
+        for i = 1, fn.winnr("$") do
+            local win = fn.win_getid(i) ---@type integer
+            if vim.b[api.nvim_win_get_buf(win)].nvim_undotree then
+                api.nvim_win_call(win, function()
+                    local cur_line = fn.line(".") ---@type integer
+                    if not (fn.line("w0") <= cur_line and cur_line <= fn.line("w$")) then
+                        api.nvim_cmd({ cmd = "norm", args = { "zz" }, bang = true }, {})
+                    end
+                end)
+            end
+        end
+    end,
+})
--- lua/mjm/undotree.lua
+++ lua/mjm/undotree.lua
@@ -2,6 +2,9 @@ local api = vim.api
 
 vim.api.nvim_cmd({ cmd = "packadd", args = { "nvim.undotree" }, bang = true }, {})
 
+-- PR: The nvim_is_undotree b:var seems unnecessary with the nvim-undotree filetype
+-- PR: Returns true in a couple places, but not at the end, and no annotation
+
 vim.keymap.set("n", "<leader>u", function()
     local win_width = api.nvim_win_get_width(api.nvim_get_current_win()) ---@type integer
     local open_width = math.floor(win_width * 0.3) ---@type integer
