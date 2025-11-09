5be9591ab3512e0028e2060dace575505783238f
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-11-08 23:53:56 -0500

    feat(plugins): map conform format with localleader. consolidate autocmds

--- lua/mjm/plugins/conform.lua
+++ lua/mjm/plugins/conform.lua
@@ -1,5 +1,4 @@
 local api = vim.api
-local expr_group = "conform-formatexpr" ---@type string
 local ft_config = {
     css = { "prettier" },
     go = { "gofumpt" },
@@ -17,33 +16,33 @@ local fts = vim.tbl_keys(ft_config) ---@type string[]
 return {
     "stevearc/conform.nvim",
     ft = fts,
-    opts = {
-        formatters_by_ft = ft_config,
-    },
+    opts = { formatters_by_ft = ft_config },
     init = function()
         api.nvim_create_autocmd("FileType", {
             group = api.nvim_create_augroup("conformer", { clear = true }),
             pattern = fts,
             callback = function(ev)
+                local do_conform = function(buf)
+                    require("conform").format({
+                        bufnr = buf,
+                        lsp_fallback = false,
+                        async = false,
+                        timeout_ms = 1000,
+                    })
+                end
+
+                vim.keymap.set("n", "<localleader>c", function()
+                    do_conform(ev.buf)
+                end, { buffer = ev.buf })
+
                 api.nvim_create_autocmd("BufWritePre", {
                     group = api.nvim_create_augroup("conform-" .. tostring(ev.buf), {}),
                     buffer = ev.buf,
                     callback = function()
-                        require("conform").format({
-                            bufnr = ev.buf,
-                            lsp_fallback = false,
-                            async = false,
-                            timeout_ms = 1000,
-                        })
+                        do_conform(ev.buf)
                     end,
                 })
-            end,
-        })
 
-        api.nvim_create_autocmd("FileType", {
-            group = api.nvim_create_augroup(expr_group, { clear = true }),
-            pattern = fts,
-            callback = function(ev)
                 local expr = "v:lua.require'conform'.formatexpr()" ---@type string
                 api.nvim_set_option_value("formatexpr", expr, { buf = ev.buf })
             end,
