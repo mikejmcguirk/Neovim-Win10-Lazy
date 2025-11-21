a3f857f288207fccd35c56d1b679dd1392a225a5
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-11-21 15:40:16 -0500

    refactor(img-clip): chang load config and mapping

--- lua/mjm/plugins/img-clip.lua
+++ lua/mjm/plugins/img-clip.lua
@@ -1,10 +1,9 @@
+local api = vim.api
 local fs = vim.fs
 local img_dir = "assets/img" ---@type string
 
 return {
     "HakonHarnes/img-clip.nvim",
-    -- event = "VeryLazy",
-    keys = { { "<leader>cp", "<cmd>PasteImage<cr>", mode = "n" } },
     opts = {
         default = {
             dir_path = img_dir,
@@ -13,7 +12,7 @@ return {
                 local root = vim.uv.cwd() ---@type string|nil
                 if not root then return end
                 local basename = fs.basename(vim.api.nvim_buf_get_name(0)) ---@type string
-                local name = require("mjm.utils").fname_root(basename, false) ---@type string
+                local name = vim.fn.fnamemodify(basename, ":r") ---@type string
                 local pattern = fs.joinpath(root, img_dir, name) .. "*" ---@type string
 
                 local files = vim.fn.glob(pattern, false, true) ---@type string[]
@@ -24,4 +23,15 @@ return {
         },
         filetypes = { markdown = { template = "![$LABEL]($FILE_PATH)" } },
     },
+    init = function()
+        api.nvim_create_autocmd("FileType", {
+            group = api.nvim_create_augroup("mjm-map-img-clip", {}),
+            pattern = "markdown",
+            callback = function(ev)
+                vim.keymap.set("n", "<localleader>p", function()
+                    api.nvim_cmd({ cmd = "PasteImage" }, {})
+                end, { buffer = ev.buf })
+            end,
+        })
+    end,
 }
