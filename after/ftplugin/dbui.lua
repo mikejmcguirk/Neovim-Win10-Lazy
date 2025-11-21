e35c33efe0be9349e0b41df0f5451556057904a2
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2025-11-21 11:07:47 -0500

    refactor(dbui): pr went through

--- after/ftplugin/dbui.lua
+++ after/ftplugin/dbui.lua
@@ -1,7 +1,2 @@
--- The plugin sets locals after filetype. Schedule here to override
--- PR: Fix this
--- TODO: This PR is open
-vim.schedule(function()
-    vim.api.nvim_set_option_value("nu", true, { scope = "local" })
-    vim.api.nvim_set_option_value("rnu", true, { scope = "local" })
-end)
+vim.api.nvim_set_option_value("nu", true, { scope = "local" })
+vim.api.nvim_set_option_value("rnu", true, { scope = "local" })
