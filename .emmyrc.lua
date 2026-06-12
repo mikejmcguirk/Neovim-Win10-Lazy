6318ca24e12a9244a26dbf40006a3d4fedd69805
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2026-06-12 16:35:59 -0400

    refactor:possible emmylua_ls switch

---
+++ .emmyrc.lua
@@ -0,0 +1,28 @@
+-- NOTE: This file is overridden by Nvim's internal configuration.
+
+return {
+    -- runtime = {
+    --     version = "LuaJIT",
+    -- },
+    -- runtime = {
+    --     require_pattern = {
+    --         "lua/?.lua",
+    --         "lua/?/init.lua",
+    --         "?/lua/?.lua",
+    --         "?/lua/?/init.lua",
+    --     },
+    -- },
+    -- diagnostics = {
+    --     disable = { "unnecessary-if" },
+    -- },
+    workspace = {
+        library = { "$VIMRUNTIME" },
+        ignoreGlobs = { "**/*_spec.lua" },
+    },
+}
+
+-- MID: For whatever reason, this is the only way I can get it to pick up the vim runtime.
+-- Maybe for the JSON version you need to export it as a global env variable, but it doesn't work
+-- there even if I enter the path manually. And if I do vim.env.VIMRUNTIME in LSP config, it
+-- fails to load (code lenses never display), but goto definition just hangs forever. This feels
+-- like some kind of underlying issue that's worth understanding.
---
+++ after/lsp/emmylua.lua
@@ -0,0 +1,13 @@
+return {
+    cmd = { "emmylua_ls" },
+    -- cmd = {
+    --   '/Users/lewrus01/projects/emmylua-analyzer-rust/target/release/emmylua_ls',
+    --    -- '--log-level', 'debug',
+    -- },
+    filetypes = { "lua" },
+    root_markers = {
+        { ".emmyrc.lua", ".emmyrc.json" },
+        ".git",
+    },
+    workspace_required = true,
+}
