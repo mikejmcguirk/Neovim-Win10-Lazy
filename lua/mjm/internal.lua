1e52c8c9a3bda56447cbaefe2ff5c165e9571394
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2026-08-09 15:40:37 -0400

    refactor:cut off farsight

--- lua/mjm/internal.lua
+++ lua/mjm/internal.lua
@@ -1,18 +1,6 @@
 local api = vim.api
 local keymap = vim.keymap
 
-require("farsight.plugin")
-
-keymap.set({ "n", "x", "o" }, "s", function()
-    print("Use ;")
-end)
-
-keymap.set({ "n", "x", "o" }, "S", function()
-    print("Use ,")
-end)
-
---------------
-
 local herder = require("qf-herder")
 herder.config({
     keymap = {
